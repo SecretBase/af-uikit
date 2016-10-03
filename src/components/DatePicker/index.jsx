3df3951a29e24147e7ec0c091837568f177d3c4a
Hans Lee <hans@aftership.net>
2016-10-03 18:08:21 +0800

    first commit

---
+++ src/components/DatePicker/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './datepicker.scss';
+
+export default (props) => {
+	return (
+		<div>hihihi</div>
+	);
+};
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import DatePicker from './components/DatePicker';
+
+
+render(<DatePicker />, window.document.getElementById('app'));
